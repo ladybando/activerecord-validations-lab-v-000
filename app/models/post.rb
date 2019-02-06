058e65d58f389a710a15e80520ce7365f3365e68
Ayana Bando <ayanaflax@yahoo.com>
2019-02-06 22:16:55 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,11 @@
 class Post < ActiveRecord::Base
   
-  validates :title, :content, presence: true
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   validates :summary, length: { maximum: 250,
     too_long: "%{count} characters is the maximum allowed" }
   validates :content, length: { minimum: 250,
     too_short: "%{count} characters is the minimum allowed" }
-  validates :title, presence: true, if: -> { clickbait}
+  -validates :title, presence: true, if: -> { clickbait}
 
     
  def clickbait
